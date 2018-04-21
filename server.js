a0742b5090822d9c277d4d1885eaae8c14081349
Arun Tej <aruntej@outlook.com>
2018-04-21 15:25:21 -0700

    Added start script and port

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 let express = require('express');
+
+const port = process.env.PORT || 80;
 let app = express();
+
 app.use('/', express.static(__dirname + '/public'));
-app.listen(80, () => {
-    console.log('295 UI server running on port '+ 80);
+
+app.listen(port, () => {
+    console.log('295 UI server running on port '+ port);
 });
